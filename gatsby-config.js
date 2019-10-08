1031d1bfafc34982c735c9c018498053e83b44b8
Gabe <gkirkley@artsclub.com>
2019-10-08 14:27:31 -0700

    Testing changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `A Netlify Test`,
+    author: `Gabe Kirkley`,
+    description: `A little test of the Netlify CMS.`,
+    siteUrl: `https://angry-brattain-90bfc7.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `gabekirkley`,
     },
   },
   plugins: [
